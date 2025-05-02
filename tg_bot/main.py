f2afa90dbe35c78b4ac914185c716c225ab98f33
el0xren <el0xren@gmail.com>
2025-05-02 11:00:36 +0000

    main: send message after booting

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -3,7 +3,14 @@ from tg_bot.core.bot import Bot
 from tg_bot.core.logging import LOGI
 
 def main():
-	bot = Bot(get_config("BOT_API_TOKEN"))
-	LOGI(f"GalaxyGeeksBot started, version {__version__}")
-	LOGI(f"Bot username: @{bot.updater.bot.get_me().username}")
-	bot.updater.start_polling()
+    bot = Bot(get_config("BOT_API_TOKEN"))
+    support_chat_id = int(get_config("SUPPORT_CHAT_ID"))
+    LOGI(f"GalaxyGeeksBot started, version {__version__}")
+    LOGI(f"Bot username: @{bot.updater.bot.get_me().username}")
+    bot.updater.bot.send_message(
+        chat_id=support_chat_id,
+        text=f"🤖 GalaxyGeeksBot v{__version__} has started successfully!",
+        disable_web_page_preview=True,
+    )
+    bot.updater.start_polling()
+
